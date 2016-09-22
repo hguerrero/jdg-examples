c029f98179cdec6f1799489539f9a6a33edb6a13
hguerrero <hguerrer@localhost>
2016-09-22 17:47:21 -0500

    model + serialization context

--- remote-query-cache/src/main/java/mx/redhat/samples/datagrid/RemoteCacheQuery.java
+++ remote-query-cache/src/main/java/mx/redhat/samples/datagrid/RemoteCacheQuery.java
@@ -1,22 +1,55 @@
 package mx.redhat.samples.datagrid;
 
+import java.io.IOException;
+
+import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
+import org.infinispan.protostream.SerializationContext;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
+import org.infinispan.protostream.annotations.ProtoSchemaBuilderException;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+
+import mx.redhat.samples.datagrid.model.Persona;
 
 public class RemoteCacheQuery 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws ProtoSchemaBuilderException, IOException 
 	{
 	    ConfigurationBuilder builder = new ConfigurationBuilder();
 	    builder.addServer()
-	    	.host("localhost")
-	    	.port(11223)
+	    		.host("localhost")
+	    		.port(11223)
 	    	.marshaller(new ProtoStreamMarshaller()); // The Protobuf based marshaller is required for query capabilities
 	    
 		RemoteCacheManager remoteCacheManager = new RemoteCacheManager(builder.build());
 
+		SerializationContext serCtx = ProtoStreamMarshaller.getSerializationContext(remoteCacheManager);
+		
+		// generate and register a Protobuf schema and marshallers based 
+		// on CachedValue class
+		ProtoSchemaBuilder protoSchemaBuilder = new ProtoSchemaBuilder();
+		
+		String generatedSchemaFile = protoSchemaBuilder
+			    .fileName("persona.proto")
+			    .packageName("samples_datagrid")
+			    .addClass(Persona.class)
+			    .build(serCtx);
+		
+		assert(serCtx.canMarshall(Persona.class));
+		
+		// display the schema file
+		System.out.println(generatedSchemaFile);
+		
+		RemoteCache<String, String> metadataCache = remoteCacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+		metadataCache.put("cachedvalue.proto", generatedSchemaFile);
+		String errors = metadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
+		if (errors != null) {
+			throw new IllegalStateException("Some Protobuf schema files contain errors:\n" + errors);
+		}
+		
 	}
 
 }
---
+++ remote-query-cache/src/main/java/mx/redhat/samples/datagrid/model/Persona.java
@@ -0,0 +1,71 @@
+package mx.redhat.samples.datagrid.model;
+
+import java.io.Serializable;
+
+import org.infinispan.protostream.annotations.ProtoDoc;
+import org.infinispan.protostream.annotations.ProtoField;
+
+@ProtoDoc("@Indexed")
+public class Persona implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	
+	private String persona;
+	private String nombre;
+	private String apellido_paterno;
+	private String apellido_materno;
+	private String rfc;
+//	private Date fecha_nacimiento;
+	
+	public Persona() {}
+	
+	public Persona(String persona, 
+			String nombre, 
+			String apellido_paterno, 
+			String apellido_materno, 
+			String rfc) 
+	{
+		this.persona = persona;
+		this.nombre = nombre;
+		this.apellido_paterno = apellido_paterno;
+		this.apellido_materno = apellido_materno;
+		this.rfc = rfc;
+	}
+	
+	@ProtoField(number = 1, required = true)
+	public String getPersona() {
+		return persona;
+	}
+	public void setPersona(String persona) {
+		this.persona = persona;
+	}
+	@ProtoField(number = 2, required = true)
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	@ProtoField(number = 3, required = true)
+	public String getApellidoPaterno() {
+		return apellido_paterno;
+	}
+	public void setApellidoPaterno(String apellido_paterno) {
+		this.apellido_paterno = apellido_paterno;
+	}
+	@ProtoField(number = 4, required = true)
+	public String getApellidoMaterno() {
+		return apellido_materno;
+	}
+	public void setApellidoMaterno(String apellido_materno) {
+		this.apellido_materno = apellido_materno;
+	}
+	@ProtoField(number = 5, required = true)
+	public String getRFC() {
+		return rfc;
+	}
+	public void setRFC(String rfc) {
+		this.rfc = rfc;
+	}
+
+}
