a80abba161d4945f68dd04c59b6b90f207ebb6c8
hguerrero <hguerrer@localhost>
2016-09-22 18:35:07 -0500

    query

--- remote-query-cache/src/main/java/mx/redhat/samples/datagrid/RemoteCacheQuery.java
+++ remote-query-cache/src/main/java/mx/redhat/samples/datagrid/RemoteCacheQuery.java
@@ -1,14 +1,18 @@
 package mx.redhat.samples.datagrid;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.Search;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 import org.infinispan.protostream.SerializationContext;
 import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
 import org.infinispan.protostream.annotations.ProtoSchemaBuilderException;
+import org.infinispan.query.dsl.Query;
+import org.infinispan.query.dsl.QueryFactory;
 import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
 
 import mx.redhat.samples.datagrid.model.Persona;
@@ -52,8 +56,25 @@ public class RemoteCacheQuery
 		
 		RemoteCache<String, Persona> remoteCache = remoteCacheManager.getCache("test");
 		
-		Persona persona1 = new Persona("0002","Juan", "Perez", "Perez", "PEPJ080808");
-		remoteCache.put(persona1.getPersona(), persona1);
+//		Persona persona1 = new Persona("0001","Juan", "Perez", "Perez", "PEPJ080808");
+//		remoteCache.putIfAbsent(persona1.getPersona(), persona1);
+//
+//		Persona persona2 = new Persona("0002","Juanita", "Perez", "Perez", "PEPJ020202");
+//		remoteCache.put(persona2.getPersona(), persona2);
+//
+//		Persona persona3 = new Persona("0003","John", "Smith", "Smith", "SMSJ070707");
+//		remoteCache.put(persona3.getPersona(), persona3);
+
+		QueryFactory<Query> qf = Search.getQueryFactory(remoteCache);
+		Query query = qf.from(Persona.class)
+				.having("nombre").like("Juan").toBuilder()
+				.build();
+
+		List<Persona> results = query.list();
+		System.out.println("Found " + results.size() + " matches:");
+		for (Persona p : results) {
+			System.out.println(">> " + p.getPersona());
+		}
 
 	}
 
